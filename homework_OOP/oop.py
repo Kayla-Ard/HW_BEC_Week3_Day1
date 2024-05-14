45f955b47b5f7ab8bc71f24d1c1a22e0b5497a4d
Kayla-Ard <kaylard@yahoo.com>
2024-05-13 18:38:45 -0600

    HW assignments

--- homework_OOP/oop.py
+++ homework_OOP/oop.py
@@ -35,18 +35,24 @@ vehicle1.update_owner('Ricky')
 
 
 # Task 2: Event Management System Enhancement
-
 # Problem Statement: Extend an existing Event class by adding a feature to keep track of the number of participants.
 # Implement a method add_participant that increases the count and a method get_participant_count to retrieve the current count.
 # Code Example: Basic Event class without participant tracking.
 
 class Event:
-    def init(self, name, date):
+    def __init__(self, name, date, part):
         self.name = name
         self.date = date
+        self.participants = part
         
-    def add_participant():
-        pass
+    def add_participant(self):
+        self.participants += 1
+        print("One participant was added!")
     
-    def get_participant():
-        pass
+    def get_participant_count(self):
+        print(f"The current participant count is {self.participants}")
+    
+participants = Event(part = 50, name = "Susie", date = "Feb 10")
+participants.add_participant()
+participants.add_participant()
+participants.get_participant_count()
